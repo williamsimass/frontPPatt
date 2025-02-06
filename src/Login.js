c537088fb721611d31a50f9c433d53790f784ffd
William Simas <94724763+williamsimass@users.noreply.github.com>
2025-02-06 16:42:28 -0300

    tes
    test

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Form, Input, Button, message } from 'antd';
 import logo from './assets/fundo.png'; // Certifique-se de que o caminho está correto
-import '../Login.css'; // Arquivo CSS para estilos personalizados
+import './Login.css'; // Arquivo CSS para estilos personalizados
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
