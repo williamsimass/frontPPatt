7d5fb8267491ec058a35b72f6107bdc931460ec5
William Simas <94724763+williamsimass@users.noreply.github.com>
2025-02-06 16:57:39 -0300

    funf
    aa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Layout } from 'antd';
 import SidebarComponent from './components/SidebarComponent';
 import HeaderComponent from './components/HeaderComponent';
+import Login from './Login'; // Importe o componente de login
 import Home from './pages/Home';
 import HistoricoRelatorios from './pages/HistoricoRelatorios';
 import SeparacaoPorCliente from './pages/SeparacaoPorCliente';
@@ -15,7 +16,7 @@ const App = () => {
     // Redireciona para a página correspondente ao item clicado
     switch (key) {
       case 'dados':
-        window.location.href = '/';
+        window.location.href = '/home';
         break;
       case 'historico':
         window.location.href = '/historico';
@@ -30,19 +31,57 @@ const App = () => {
 
   return (
     <Router>
-      <Layout style={{ minHeight: '100vh' }}>
-        <SidebarComponent onMenuClick={handleMenuClick} />
-        <Layout>
-          <HeaderComponent />
-          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/historico" element={<HistoricoRelatorios />} />
-              <Route path="/separacao" element={<SeparacaoPorCliente />} />
-            </Routes>
-          </Content>
-        </Layout>
-      </Layout>
+      <Routes>
+        {/* Rota para a tela de login */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Rota padrão redireciona para o login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Rotas protegidas (após o login) */}
+        <Route
+          path="/home"
+          element={
+            <Layout style={{ minHeight: '100vh' }}>
+              <SidebarComponent onMenuClick={handleMenuClick} />
+              <Layout>
+                <HeaderComponent />
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                  <Home />
+                </Content>
+              </Layout>
+            </Layout>
+          }
+        />
+        <Route
+          path="/historico"
+          element={
+            <Layout style={{ minHeight: '100vh' }}>
+              <SidebarComponent onMenuClick={handleMenuClick} />
+              <Layout>
+                <HeaderComponent />
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                  <HistoricoRelatorios />
+                </Content>
+              </Layout>
+            </Layout>
+          }
+        />
+        <Route
+          path="/separacao"
+          element={
+            <Layout style={{ minHeight: '100vh' }}>
+              <SidebarComponent onMenuClick={handleMenuClick} />
+              <Layout>
+                <HeaderComponent />
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                  <SeparacaoPorCliente />
+                </Content>
+              </Layout>
+            </Layout>
+          }
+        />
+      </Routes>
     </Router>
   );
 };
