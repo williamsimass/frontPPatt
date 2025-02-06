31edd05794bda41888c29265ddc4be0b4d5a4c37
William Simas <94724763+williamsimass@users.noreply.github.com>
2025-02-06 16:45:01 -0300

    att
    att1

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Menu } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { DatabaseOutlined, FileTextOutlined, TeamOutlined } from '@ant-design/icons';
 import { Layout } from 'antd';
 
 const { Sider } = Layout;
@@ -10,18 +10,18 @@ const SidebarComponent = ({ onMenuClick }) => {
     <Sider width={250} style={{ background: 'white', padding: '16px' }}>
       <Menu 
         mode="inline" 
-        defaultSelectedKeys={['sub1']} 
+        defaultSelectedKeys={['dados']} 
         style={{ height: '100%', borderRight: 0, marginTop: '16px' }}
         onClick={(e) => onMenuClick(e.key)}
       >
-        <Menu.Item key="sub1" icon={<UserOutlined />} disabled>
-          Autores
+        <Menu.Item key="dados" icon={<DatabaseOutlined />}>
+          Dados do Banco de Dados
         </Menu.Item>
-        <Menu.Item key="sub2" icon={<LaptopOutlined />} disabled>
-          Processos
+        <Menu.Item key="historico" icon={<FileTextOutlined />}>
+          Histórico de Relatórios
         </Menu.Item>
-        <Menu.Item key="sub3" icon={<NotificationOutlined />} disabled>
-          Detalhes
+        <Menu.Item key="separacao" icon={<TeamOutlined />}>
+          Separação por Cliente
         </Menu.Item>
       </Menu>
     </Sider>
