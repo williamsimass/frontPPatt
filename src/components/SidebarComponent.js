897b374d0ac3aa44ad62ca4c03528d8fcbbeabd2
William Simas <94724763+williamsimass@users.noreply.github.com>
2025-02-07 15:26:54 -0300

    att sidebar
    att

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import { Menu } from 'antd';
-import { DatabaseOutlined, FileTextOutlined, TeamOutlined } from '@ant-design/icons';
-import { Layout } from 'antd';
+import React, { useState } from 'react';
+import { Menu, Layout } from 'antd';
+import { DatabaseOutlined, FileTextOutlined, TeamOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 
 const { Sider } = Layout;
 
 const SidebarComponent = ({ onMenuClick }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <Sider width={250} style={{ background: 'white', padding: '16px' }}>
+    <Sider 
+      width={250} 
+      collapsible 
+      collapsed={collapsed} 
+      onCollapse={setCollapsed}
+      style={{ background: '#fff', padding: '16px', borderRight: '1px solid #ddd' }}
+    >
+      <div 
+        style={{ textAlign: 'center', marginBottom: '16px', cursor: 'pointer' }} 
+        onClick={() => setCollapsed(!collapsed)}
+      >
+        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      </div>
+
       <Menu 
         mode="inline" 
         defaultSelectedKeys={['dados']} 
         style={{ height: '100%', borderRight: 0, marginTop: '16px' }}
         onClick={(e) => onMenuClick(e.key)}
       >
-        <Menu.Item key="dados" icon={<DatabaseOutlined />}>
+        <Menu.Item key="dados" icon={<DatabaseOutlined />} aria-label="Dados do Banco de Dados">
           Dados do Banco de Dados
         </Menu.Item>
-        <Menu.Item key="historico" icon={<FileTextOutlined />}>
+        <Menu.Item key="historico" icon={<FileTextOutlined />} aria-label="Histórico de Relatórios">
           Histórico de Relatórios
         </Menu.Item>
-        <Menu.Item key="separacao" icon={<TeamOutlined />}>
+        <Menu.Item key="separacao" icon={<TeamOutlined />} aria-label="Separação por Cliente">
           Separação por Cliente
         </Menu.Item>
       </Menu>
