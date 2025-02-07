df58d2fbc7b4ed0b2922435a68fdebe20be0dcb9
William Simas <94724763+williamsimass@users.noreply.github.com>
2025-02-07 15:49:21 -0300

    att
    att

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import Login from './Login'; // Login component
-import Home from './pages/Home'; // Main home page component
+import AppRoutes from './routes'; // Importando o arquivo de rotas
+import SidebarComponent from './components/SidebarComponent';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} /> {/* Redirects to login */}
-      <Route path="/login" element={<Login />} /> {/* Login page */}
-      <Route path="/home" element={<Home />} /> {/* Main page */}
-      {/* Consider renaming or removing /full route if not used */}
-      <Route path="/full" element={<Home />} /> {/* Possibly redundant */}
-    </Routes>
+    <div style={{ display: 'flex' }}>
+      <SidebarComponent />
+      <div style={{ marginLeft: 250, width: '100%' }}>
+        <AppRoutes /> {/* Exibindo as rotas */}
+      </div>
+    </div>
   );
 };
 
